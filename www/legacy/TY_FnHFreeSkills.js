60917f092ed8f51f4b6c776752715aa141efb6b1
TobyYasha <84013830+TobyYasha@users.noreply.github.com>
2023-11-29 22:53:17 +0200

    Update TY_FnHFreeSkills.js

--- www/legacy/TY_FnHFreeSkills.js
+++ www/legacy/TY_FnHFreeSkills.js
@@ -23,16 +23,29 @@
 		{ skillId: 450, switchIds: [1996] }, // Blood sacrifice
 		{ skillId: 146, switchIds: [1186, 1964] }, // En garde
 		{ skillId: 146, switchIds: [1986] }, // Burry the trauma
-		{ skillId: 476, switchIds: [ ] }, // Order, charge!
-		{ skillId: 446, switchIds: [ ] }, // Greater meditation
-		{ skillId: 338, switchIds: [ ] }, // meditation
-		{ skillId: 447, switchIds: [ ] }, // spice forge
-		{ skillId: 444, switchIds: [ ] }, // La danse macabre
-		{ skillId: 472, switchIds: [ ] }, // Explosives
-		{ skillId: 471, switchIds: [ ] }, // Killing intent
-		{ skillId: 470, switchIds: [ ] }, // intimidate
-		{ skillId: 473, switchIds: [ ] }, // steal
-		{ skillId: 798, switchIds: [ ] }, // red arc
+		{ skillId: 476, switchIds: [1990] }, // Order, charge!
+		{ skillId: 446, switchIds: [2008] }, // Greater meditation
+		{ skillId: 338, switchIds: [2006] }, // meditation
+		{ skillId: 447, switchIds: [2212] }, // spice forge
+		{ skillId: 444, switchIds: [2002] }, // La danse macabre
+		{ skillId: 472, switchIds: [2018] }, // Explosives -- gain item 250 for recipe
+		{ skillId: 471, switchIds: [2014] }, // Killing intent
+		{ skillId: 470, switchIds: [2010] }, // intimidate
+		{ skillId: 473, switchIds: [2012] }, // steal
+		{ skillId: 798, switchIds: [1455] }, // red arc
+		{ skillId: 335, switchIds: [1246] }, // engrave
+		{ skillId: 323, switchIds: [1924] }, // gunproficiency
+		{ skillId: 441, switchIds: [1926] }, // warding_sigil
+		{ skillId: 487, switchIds: [1932] }, // diagnosis
+		{ skillId: 337, switchIds: [1248] }, // advanced_occultism
+		{ skillId: 367, switchIds: [1541] }, // medicinal
+		{ skillId: 366, switchIds: [] }, // organ harvest(with medicinal)
+		{ skillId: 454, switchIds: [1938] }, // slow_metabolism
+		// TO DO: Make the below skills work
+		/*
+		{ skillId: 0,   switchIds: [1982] }, // state m.defense +1 -- 206 | state m.defense +2 -- 207
+		{ skillId: 0,   switchIds: [1984] }, // change m.attack param by +1
+ 		 */
 	];
 	
 	//==========================================================
