15ac8d92d6dda1294b0e586406ea85f8eed26805
TobyYasha <84013830+TobyYasha@users.noreply.github.com>
2024-04-30 22:05:03 +0300

    Update TY_FnHInfiniteStamina.js

--- www/legacy/TY_FnHInfiniteStamina.js
+++ www/legacy/TY_FnHInfiniteStamina.js
@@ -13,29 +13,80 @@
 		// Mod Configurations -- 
 	//==========================================================
 		
+	let autoDashToggle = false;
+
 	// Check if it's a common event for the stamina bar graphic
 	function isStaminaCommonEvent(commonEvent) {
+		const commonEventIds = [573, 574, 575];
 		if (commonEvent) {
-			return [573, 574, 575].includes(commonEvent.id);
+			return commonEventIds.includes(commonEvent.id);
 		} else {
 			return false;
+		}
+	}
+
+	/*
+	SWITCH DEFINITION:
+		202 -  Suicide in Process
+		203 -  Climbing
+		403 -  Love Dark Priest x Mercenary (Where's this used?)
+		434 -  Pig Death Scene
+		2420 - Hexen GFX
+	*/
+	function meetsDashSwitchCondition() {
+		const switchIds = [202, 203, 403, 434, 2420];
+		return switchIds.every(switchId => !$gameSwitches.value(switchId));
+	}
+
+	/*
+	VARIABLE DEFINITION:
+		868 -  Burn
+		2802 - One leg restriction
+	*/
+	function meetsDashVariableCondition() {
+		const variableIds = [868, 2802];
+		return variableIds.every(variableId => !$gameVariables.value(variableId));
+	}
+
+	function meetsDashConditions() {
+		return (
+			$gamePlayer.isMoving() &&
+			meetsDashSwitchCondition() &&
+			meetsDashVariableCondition()
+		);
+	}
+
+	function updateDashToggle() {
+		if (Input.isTriggered("shift")) {
+			autoDashToggle = !autoDashToggle;
+			updateDashState();
+		}
+	}
+
+	function updateDashState() {
+		if (autoDashToggle && meetsDashConditions()) {
+			$gameSwitches.setValue(1956, true);
+		}
+	}
+
+	// Ensures that stamina doesn't go down
+	function updateStaminaMeter() {
+		if (allowInfiniteStamina) { // if "true" then don't run out of stamina
+			$gameVariables.setValue(2714, 0);
 		}
 	}
 	
 	//==========================================================
 		// Game Configurations -- Game_Map
 	//==========================================================
-	
-	if (allowInfiniteStamina) { // if "true" then don't run out of stamina
 
-		// Reset stamina counter back to 0
-		const TY_Game_Map_Update = Game_Map.prototype.update;
-		Game_Map.prototype.update = function(sceneActive) {
-			TY_Game_Map_Update.call(this, ...arguments);
-			$gameVariables.setValue(2714, 0); // HERE
-		};
-	
-	}
+	// Update dash and stamina features
+	const TY_Game_Map_Update = Game_Map.prototype.update;
+	Game_Map.prototype.update = function(sceneActive) {
+		TY_Game_Map_Update.call(this, ...arguments);
+		updateDashToggle();
+		updateStaminaMeter();
+	};
 	
 	if (!allowDrawStaminaBar) { // if "false" then don't draw the stamina bar graphic 
 
