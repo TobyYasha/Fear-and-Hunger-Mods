e84f56547f39f31f120338ce92ee25317baec672
TobyYasha <84013830+TobyYasha@users.noreply.github.com>
2024-06-09 20:18:17 +0300

    Update TY_FnHWaitForDialogue.js

--- www/legacy/TY_FnHWaitForDialogue.js
+++ www/legacy/TY_FnHWaitForDialogue.js
@@ -62,6 +62,8 @@
 		// MapId 22 - huts_village
 		// MapId 23 - Staircase
 		// MapId 24 - level7_A
+		// MapId 26 - Mahabre1_1_A
+		// MapId 27 - Mahabre1_2_A
 		// MapId 29 - level1_B
 		// MapId 30 - level6_2_Crash
 		// MapId 31 - Level1_3_B
@@ -78,6 +80,7 @@
 		// MapId 59 - level3_C
 		// MapId 60 - thicket_4_A (not sure if used in-game)
 		// MapId 62 - Smash_pit
+		// MapId 63 - Rondon_inside2
 		// MapId 65 - Level8_2_A
 		// MapId 66 - thicket_1_C
 		// MapId 67 - thicket_2_C
@@ -89,28 +92,78 @@
 		// MapId 77 - Level2_Basement_A
 		// MapId 78 - Temple of Torment
 		// MapId 79 - Temple of Torment_merc
+		// MapId 80 - Back_alleys
 		// MapId 81 - Temple of Torment_out
 		// MapId 82 - Temple of Torment_knight
 		// MapId 83 - Temple of Torment_dark
 		// MapId 84 - Temple of Torment_captain
 		// MapId 85 - Temple of Torment_empty
+		// MapId 87 - Mahabre_inside1
 		// MapId 93 - Level8_A
 		// MapId 96 - golden_temple
+		// MapId 100 - Center_square
+		// MapId 101 - Passageway
+		// MapId 102 - Ma'habre_Tower
+		// MapId 103 - Temple_district
 		// MapId 104 - Grand_library
 		// MapId 105 - Grand_library_inner
+		// MapId 106 - pit_of_enlightenment
+		// MapId 107 - Mahabre_inside2
+		// MapId 108 - Ancient_Passageway
+		// MapId 109 - Ancient_Center_square
+		// MapId 110 - Ancient_Back_alleys
+		// MapId 111 - Ancient_1_2_A
+		// MapId 112 - Ancient_Tower
+		// MapId 113 - Ancient_Temple_district
+		// MapId 114 - Ancient_inside2
+		// MapId 115 - Ancient_inside1
+		// MapId 116 - Ancient_Grand_library
+		// MapId 117 - Ancient_Grand_library_inner
+		// MapId 118 - Ancient_pit_of_enlightenment
+		// MapId 119 - Tower of Endless
+		// MapId 120 - Tower of Endless
+		// MapId 121 - Golden_temple
 		// MapId 122 - Ancient_Temple of Torment
 		// MapId 123 - Temple of Torment_level2
 		// MapId 124 - Flipside_Basement_A
+		// MapId 125 - Golden_temple_inside
 		// MapId 126 - Ancient_Golden_temple
 		// MapId 127 - Golden_temple_inside
 		// MapId 128 - level1_2_B
+		// MapId 133 - Dream_Rondon
+		// MapId 134 - Rondon_inside
+		// MapId 135 - Dream_Oldegård
+		// MapId 136 - Oldegård_house
+		// MapId 139 - Ancient_cave_entrance
 		// MapId 141 - deeper_thicket
 		// MapId 142 - Level 9
+		// MapId 143 - Ancient_cave
 		// MapId 145 - Level9_trap
+		// MapId 146 - Golden_temple_inside2
+		// MapId 148 - Ancient_tomb
+		// MapId 149 - Ancient_tomb_gods
+		// MapId 150 - Mahabre_tomb
+		// MapId 151 - Mahabre_tomb_gods
+		// MapId 153 - Mahabre_Ancient_cave
+		// MapId 154 - Ancient1_1_A
+		// MapId 155 - Ancient_passage
+		// MapId 160 - The_Void
 		// MapId 163 - Level3_Basement_Corridor
+		// MapId 164 - Mahabre1_2_Book
+		// MapId 165 - Caverns (school days)
+		// MapId 166 - Tombs (school days)
+		// MapId 167 - Blood pit (school days)
+		// MapId 168 - Overworld (school days)
+		// MapId 171 - Dungeons (school days)
+		// MapId 172 - Prisons (school days)
+		// MapId 174 - Mines (school days)
+		// MapId 175 - Ancient_inside
+		// MapId 177 - Inside3 (school days)
+		// MapId 181 - Prom (school days)
 		// MapId 182 - Level2_Basement_B
 		// MapId 183 - level1_D
 		// MapId 184 - level5_C
+		// MapId 185 - The_Void2
 
 		function getMiscMapEvents() { // ONLY FNH1 FOR NOW
 			const mapEvents = [
@@ -200,6 +253,16 @@
 				{ mapId: 24, eventId: 17  }, // Sanity Loss
 				{ mapId: 24, eventId: 98  }, // Sanity Gain
 
+				{ mapId: 26, eventId: 26  }, // Blood
+				{ mapId: 26, eventId: 23  }, // Bleeding
+				{ mapId: 26, eventId: 28  }, // Sanity Loss
+				{ mapId: 26, eventId: 11  }, // Mahabre Timer
+				{ mapId: 26, eventId: 12  }, // Mahabre Timer
+
+				{ mapId: 27, eventId: 26  }, // Blood
+				{ mapId: 27, eventId: 23  }, // Bleeding
+				{ mapId: 27, eventId: 28  }, // Sanity Loss
+
 				{ mapId: 29, eventId: 27  }, // Blood
 				{ mapId: 29, eventId: 29  }, // Bleeding
 				{ mapId: 29, eventId: 131 }, // Sanity Loss
@@ -276,6 +339,10 @@
 				{ mapId: 62, eventId: 54  }, // Sanity Loss
 				{ mapId: 62, eventId: 58  }, // Sanity Gain
 
+				{ mapId: 63, eventId: 9   }, // Blood
+				{ mapId: 63, eventId: 13  }, // Bleeding
+				{ mapId: 63, eventId: 148 }, // Sanity Loss
+
 				{ mapId: 65, eventId: 9   }, // Blood
 				{ mapId: 65, eventId: 13  }, // Bleeding
 				{ mapId: 65, eventId: 148 }, // Sanity Loss
@@ -326,6 +393,11 @@
 				{ mapId: 79, eventId: 23  }, // Bleeding
 				{ mapId: 79, eventId: 147 }, // Sanity Loss
 
+				{ mapId: 80, eventId: 26  }, // Blood
+				{ mapId: 80, eventId: 23  }, // Bleeding
+				{ mapId: 80, eventId: 28  }, // Sanity Loss
+				{ mapId: 80, eventId: 418 }, // Sanity Gain
+
 				{ mapId: 81, eventId: 26  }, // Blood
 				{ mapId: 81, eventId: 23  }, // Bleeding
 				{ mapId: 81, eventId: 147 }, // Sanity Loss
@@ -346,6 +418,10 @@
 				{ mapId: 85, eventId: 23  }, // Bleeding
 				{ mapId: 85, eventId: 147 }, // Sanity Loss
 
+				{ mapId: 87, eventId: 26  }, // Blood
+				{ mapId: 87, eventId: 23  }, // Bleeding
+				{ mapId: 87, eventId: 147 }, // Sanity Loss
+
 				{ mapId: 93, eventId: 9   }, // Blood
 				{ mapId: 93, eventId: 13  }, // Bleeding
 				{ mapId: 93, eventId: 148 }, // Sanity Loss
@@ -354,6 +430,23 @@
 				{ mapId: 96, eventId: 23  }, // Bleeding
 				{ mapId: 96, eventId: 28  }, // Sanity Gain
 
+				{ mapId: 100, eventId: 9   }, // Blood
+				{ mapId: 100, eventId: 13  }, // Bleeding
+				{ mapId: 100, eventId: 148 }, // Sanity Loss
+
+				{ mapId: 101, eventId: 9   }, // Blood
+				{ mapId: 101, eventId: 13  }, // Bleeding
+				{ mapId: 101, eventId: 51  }, // Sanity Loss
+				{ mapId: 101, eventId: 138 }, // Sanity Loss (yeah..there are 2 sanity lose events)
+
+				{ mapId: 102, eventId: 9   }, // Blood
+				{ mapId: 102, eventId: 13  }, // Bleeding
+				{ mapId: 102, eventId: 126 }, // Sanity Loss
+
+				{ mapId: 103, eventId: 9   }, // Blood
+				{ mapId: 103, eventId: 13  }, // Bleeding
+				{ mapId: 103, eventId: 138 }, // Sanity Loss
+
 				{ mapId: 104, eventId: 9   }, // Blood
 				{ mapId: 104, eventId: 13  }, // Bleeding
 				{ mapId: 104, eventId: 148 }, // Sanity Loss
@@ -361,6 +454,71 @@
 				{ mapId: 105, eventId: 9   }, // Blood
 				{ mapId: 105, eventId: 13  }, // Bleeding
 				{ mapId: 105, eventId: 148 }, // Sanity Loss
+
+				{ mapId: 106, eventId: 9   }, // Blood
+				{ mapId: 106, eventId: 13  }, // Bleeding
+				{ mapId: 106, eventId: 148 }, // Sanity Loss
+
+				{ mapId: 107, eventId: 26  }, // Blood
+				{ mapId: 107, eventId: 23  }, // Bleeding
+				{ mapId: 107, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 108, eventId: 9   }, // Blood
+				{ mapId: 108, eventId: 13  }, // Bleeding
+				{ mapId: 108, eventId: 138 }, // Sanity Loss
+
+				{ mapId: 109, eventId: 9   }, // Blood
+				{ mapId: 109, eventId: 13  }, // Bleeding
+				{ mapId: 109, eventId: 138 }, // Sanity Loss
+				{ mapId: 109, eventId: 148 }, // Sanity Gain
+
+				{ mapId: 110, eventId: 26  }, // Blood
+				{ mapId: 110, eventId: 23  }, // Bleeding
+				{ mapId: 110, eventId: 28  }, // Sanity Gain
+
+				{ mapId: 111, eventId: 26  }, // Blood
+				{ mapId: 111, eventId: 23  }, // Bleeding
+				{ mapId: 111, eventId: 28  }, // Sanity Gain
+
+				{ mapId: 112, eventId: 9   }, // Blood
+				{ mapId: 112, eventId: 13  }, // Bleeding
+				{ mapId: 112, eventId: 112 }, // Sanity Loss
+
+				{ mapId: 113, eventId: 9   }, // Blood
+				{ mapId: 113, eventId: 13  }, // Bleeding
+				{ mapId: 113, eventId: 138 }, // Sanity Loss
+
+				{ mapId: 114, eventId: 26  }, // Blood
+				{ mapId: 114, eventId: 23  }, // Bleeding
+				{ mapId: 114, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 115, eventId: 26  }, // Blood
+				{ mapId: 115, eventId: 23  }, // Bleeding
+				{ mapId: 115, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 116, eventId: 9   }, // Blood
+				{ mapId: 116, eventId: 13  }, // Bleeding
+				{ mapId: 116, eventId: 138 }, // Sanity Loss
+
+				{ mapId: 117, eventId: 9   }, // Blood
+				{ mapId: 117, eventId: 13  }, // Bleeding
+				{ mapId: 117, eventId: 117 }, // Sanity Loss
+
+				{ mapId: 118, eventId: 9   }, // Blood
+				{ mapId: 118, eventId: 13  }, // Bleeding
+				{ mapId: 118, eventId: 148 }, // Sanity Loss
+
+				{ mapId: 119, eventId: 26  }, // Blood
+				{ mapId: 119, eventId: 23  }, // Bleeding
+				{ mapId: 119, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 120, eventId: 26  }, // Blood
+				{ mapId: 120, eventId: 23  }, // Bleeding
+				{ mapId: 120, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 121, eventId: 9   }, // Blood
+				{ mapId: 121, eventId: 13  }, // Bleeding
+				{ mapId: 121, eventId: 148 }, // Sanity Loss
 
 				{ mapId: 122, eventId: 26  }, // Blood
 				{ mapId: 122, eventId: 23  }, // Bleeding
@@ -374,6 +532,10 @@
 				{ mapId: 124, eventId: 23  }, // Bleeding
 				{ mapId: 124, eventId: 147 }, // Sanity Loss
 
+				{ mapId: 125, eventId: 9   }, // Blood
+				{ mapId: 125, eventId: 13  }, // Bleeding
+				{ mapId: 125, eventId: 148 }, // Sanity Loss
+
 				{ mapId: 126, eventId: 9   }, // Blood
 				{ mapId: 126, eventId: 13  }, // Bleeding
 				{ mapId: 126, eventId: 138 }, // Sanity Loss
@@ -386,6 +548,26 @@
 				{ mapId: 128, eventId: 23  }, // Bleeding
 				{ mapId: 128, eventId: 28  }, // Sanity Gain
 
+				{ mapId: 133, eventId: 9   }, // Blood
+				{ mapId: 133, eventId: 13  }, // Bleeding
+				{ mapId: 133, eventId: 148 }, // Sanity Loss
+
+				{ mapId: 134, eventId: 9   }, // Blood
+				{ mapId: 134, eventId: 13  }, // Bleeding
+				{ mapId: 134, eventId: 148 }, // Sanity Loss
+
+				{ mapId: 135, eventId: 9   }, // Blood
+				{ mapId: 135, eventId: 13  }, // Bleeding
+				{ mapId: 135, eventId: 148 }, // Sanity Gain
+
+				{ mapId: 136, eventId: 9   }, // Blood
+				{ mapId: 136, eventId: 13  }, // Bleeding
+				{ mapId: 136, eventId: 148 }, // Sanity Loss
+
+				{ mapId: 139, eventId: 26  }, // Blood
+				{ mapId: 139, eventId: 23  }, // Bleeding
+				{ mapId: 139, eventId: 147 }, // Sanity Loss
+
 				{ mapId: 141, eventId: 26  }, // Blood
 				{ mapId: 141, eventId: 23  }, // Bleeding
 				{ mapId: 141, eventId: 104 }, // Sanity Loss
@@ -394,13 +576,101 @@
 				{ mapId: 142, eventId: 13  }, // Bleeding
 				{ mapId: 142, eventId: 148 }, // Sanity Loss
 
+				{ mapId: 143, eventId: 26  }, // Blood
+				{ mapId: 143, eventId: 23  }, // Bleeding
+				{ mapId: 143, eventId: 147 }, // Sanity Loss / Gain
+
 				{ mapId: 145, eventId: 9   }, // Blood
 				{ mapId: 145, eventId: 13  }, // Bleeding
 				{ mapId: 145, eventId: 148 }, // Sanity Loss
 
+				{ mapId: 146, eventId: 9   }, // Blood
+				{ mapId: 146, eventId: 13  }, // Bleeding
+				{ mapId: 146, eventId: 148 }, // Sanity Loss
+
+				{ mapId: 148, eventId: 26  }, // Blood
+				{ mapId: 148, eventId: 23  }, // Bleeding
+				{ mapId: 148, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 149, eventId: 26  }, // Blood
+				{ mapId: 149, eventId: 23  }, // Bleeding
+				{ mapId: 149, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 150, eventId: 26  }, // Blood
+				{ mapId: 150, eventId: 23  }, // Bleeding
+				{ mapId: 150, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 151, eventId: 26  }, // Blood
+				{ mapId: 151, eventId: 23  }, // Bleeding
+				{ mapId: 151, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 153, eventId: 26  }, // Blood
+				{ mapId: 153, eventId: 23  }, // Bleeding
+				{ mapId: 153, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 154, eventId: 26  }, // Blood
+				{ mapId: 154, eventId: 23  }, // Bleeding
+				{ mapId: 154, eventId: 28  }, // Sanity Loss
+
+				{ mapId: 155, eventId: 26  }, // Blood
+				{ mapId: 155, eventId: 23  }, // Bleeding
+				{ mapId: 155, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 160, eventId: 9   }, // Blood
+				{ mapId: 160, eventId: 13  }, // Bleeding
+				{ mapId: 160, eventId: 138 }, // Sanity Loss
+
 				{ mapId: 163, eventId: 26  }, // Blood
 				{ mapId: 163, eventId: 23  }, // Bleeding
 				{ mapId: 163, eventId: 151 }, // Sanity Loss
+
+				{ mapId: 164, eventId: 26  }, // Blood
+				{ mapId: 164, eventId: 23  }, // Bleeding
+				{ mapId: 164, eventId: 28  }, // Sanity Gain
+
+				{ mapId: 165, eventId: 22  }, // Blood
+				{ mapId: 165, eventId: 19  }, // Bleeding
+				{ mapId: 165, eventId: 187 }, // Sanity Loss
+
+				{ mapId: 166, eventId: 26  }, // Blood
+				{ mapId: 166, eventId: 23  }, // Bleeding
+				{ mapId: 166, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 167, eventId: 24  }, // Blood
+				{ mapId: 167, eventId: 20  }, // Bleeding
+				{ mapId: 167, eventId: 26  }, // Sanity Loss
+
+				{ mapId: 168, eventId: 9   }, // Blood
+				{ mapId: 168, eventId: 13  }, // Bleeding
+				{ mapId: 168, eventId: 138 }, // Sanity Loss
+				{ mapId: 168, eventId: 148 }, // Sanity Gain
+
+				{ mapId: 171, eventId: 27  }, // Blood
+				{ mapId: 171, eventId: 29  }, // Bleeding
+				{ mapId: 171, eventId: 131 }, // Sanity Loss
+				{ mapId: 171, eventId: 133 }, // Sanity Gain
+
+				{ mapId: 172, eventId: 28 }, // Blood
+				{ mapId: 172, eventId: 25 }, // Bleeding
+				{ mapId: 172, eventId: 54 }, // Sanity Loss
+				{ mapId: 172, eventId: 58 }, // Sanity Gain
+
+				{ mapId: 174, eventId: 51 }, // Blood
+				{ mapId: 174, eventId: 53 }, // Bleeding
+				{ mapId: 174, eventId: 66 }, // Sanity Loss
+				{ mapId: 174, eventId: 67 }, // Sanity Gain
+
+				{ mapId: 175, eventId: 26  }, // Blood
+				{ mapId: 175, eventId: 23  }, // Bleeding
+				{ mapId: 175, eventId: 147 }, // Sanity Loss
+
+				{ mapId: 177, eventId: 27  }, // Blood
+				{ mapId: 177, eventId: 29  }, // Bleeding
+
+				{ mapId: 181, eventId: 9  }, // Blood
+				{ mapId: 181, eventId: 13  }, // Bleeding
+				{ mapId: 181, eventId: 138 }, // Sanity Loss
+				{ mapId: 181, eventId: 148 }, // Sanity Gain
 
 				{ mapId: 182, eventId: 26  }, // Blood
 				{ mapId: 182, eventId: 23  }, // Bleeding
@@ -416,6 +686,10 @@
 				{ mapId: 184, eventId: 60  }, // Sanity Loss
 				{ mapId: 184, eventId: 61  }, // Sanity Gain
 				{ mapId: 184, eventId: 176 }, // Yellow Mage Dance
+
+				{ mapId: 185, eventId: 9   }, // Blood
+				{ mapId: 185, eventId: 13  }, // Bleeding
+				{ mapId: 185, eventId: 138 }, // Sanity Loss
 
 			]
 
